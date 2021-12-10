16e06398cc4beaddbfecf2dcd96180cc01e94812
iamshour -Git <aliahmadshour@gmail.com>
2021-12-10 10:58:27 -0800

    --

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 import Loading from "components/utility/Loading"
-import NotificationCard from "components/notifications/NotificationCard"
+import Card from "components/Notifications/Card"
 
 const Notifications = () => {
 	const { notifications, userLoading } = useSelector((state) => state.user)
@@ -14,17 +14,9 @@ const Notifications = () => {
 					{notifications?.length === 0 ? (
 						<h1>No notificaions at the moment!</h1>
 					) : (
-						notifications?.map((n) =>
-							console.log(n?.body)
-							// <NotificationCard
-							// 	key={n?.notificationId}
-							// 	body={n?.body}
-							// 	notificationId={n?.notificationId}
-							// 	referenceId={n?.referenceId}
-							// 	userId={n?.userId}
-							// 	time={n?.createdAt}
-							// />
-						)
+						notifications?.map((item) => {
+							return <Card item={item} />
+						})
 					)}
 				</div>
 			)}
